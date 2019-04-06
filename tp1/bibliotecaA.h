cc3508878e8fa3d452485740732bb28a278a7dd0
martingimenez7 <martingimenez7@hotmail.com>
2019-04-05 21:02:14 -0300

    .

--- tp1/bibliotecaA.h
+++ tp1/bibliotecaA.h
@@ -1,7 +1,45 @@
 
+/** \brief funcion para sumar dos numeros
+ *
+ * \param int primer operando
+ * \param int segundo operando
+ * \return int resultado del calculo
+ *
+ */
 int suma(int, int);
+
+/** \brief funcion para restar dos numeros
+ *
+ * \param int primer operando
+ * \param int segundo operando
+ * \return int resultado del calculo
+ *
+ */
 int resta(int, int);
+
+/** \brief funcion para dividir dos numeros
+ *
+ * \param int primer operando
+ * \param int segundo operando
+ * \return int resultado del calculo / error
+ *
+ */
 float division(int, int);
+
+/** \brief funcion para multiplicar dos numeros
+ *
+ * \param int primer operando
+ * \param int segundo operando
+ * \return int resultado del calculo
+ *
+ */
 int multiplicacion(int, int);
+
+/** \brief funcion para factoriar 1 numero
+ *
+ * \param int primer operando
+ * \return int resultado del calculo / error
+ *
+ */
 int factorial(int);
 
